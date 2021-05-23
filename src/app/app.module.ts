e4464158fe5613de79599e7dd82fdc20be58469f
Kevin Poelstra <kevin.plstr@gmail.com>
2021-05-23 12:04:26 +0200

    added debug in console, username/password accessible in component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginscreenComponent } from './loginscreen/loginscreen.component';
@@ -12,7 +12,8 @@ import { LoginscreenComponent } from './loginscreen/loginscreen.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/loginscreen/loginscreen.component.ts
+++ src/app/loginscreen/loginscreen.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder }  from '@angular/forms';
+
 
 @Component({
   selector: 'app-loginscreen',
@@ -9,10 +11,19 @@ export class LoginscreenComponent implements OnInit {
 
   usernameField?:string;
   passwordField?:string;
+  loginForm = this.formBuilder.group({
+    username: '',
+    password: ''
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  onLogin():void {
+    console.log("username: " + this.loginForm.value["username"]);
+    console.log("password: " + this.loginForm.value["password"]);
+    // do stuff to check if username/password combination is correct. get jwt if it is.
+  }
 }

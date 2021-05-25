079ae294758d63849bdf6076d59f2efbb780d430
Ew Its Mike <m.land@live.nl>
2021-05-25 15:57:19 +0200

    Basic forum-thread functionality

---
+++ src/app/forum-thread/forum-thread.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { POSTS } from '../mock-posts';
+import { Forumpost } from '../forumpost';
+import { parseSelectorToR3Selector } from '@angular/compiler/src/core';
+import { NewForumpostService } from '../new-forumpost.service';
+
+@Component({
+  selector: 'app-forum-thread',
+  templateUrl: './forum-thread.component.html',
+  styleUrls: ['./forum-thread.component.css']
+})
+export class ForumThreadComponent implements OnInit {
+
+  posts: Forumpost[] = [];
+
+  constructor(private postService: NewForumpostService) { }
+
+  ngOnInit(): void {
+    this.getPosts();
+  }
+
+  public addNewPost(post: Forumpost) : void {
+    this.posts.push(post);
+  }
+
+
+
+  getPosts(): void {
+    this.postService.getPosts().subscribe(posts => this.posts = posts);
+  }
+
+}

a4612745e094b3c85825be8aefba66f6b4eaf7f3
2zqa <25235249+2zqa@users.noreply.github.com>
2021-05-25 13:52:31 +0200

    Remove WIP LivingRoomComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { LoginscreenComponent } from './loginscreen/loginscreen.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FloorplanComponent } from './floorplan/floorplan.component';
-import { LivingroomComponent } from './livingroom/livingroom.component';
 
 @NgModule({
   declarations: [

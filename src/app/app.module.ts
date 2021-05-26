0853dda8fb5cc1122f3ebf3742a01ef0ee236d35
Naomi Verkade <57986773+pluficorn@users.noreply.github.com>
2021-05-26 14:42:30 +0200

    START

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { KitchenComponent } from './kitchen/kitchen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KitchenComponent
   ],
   imports: [
     BrowserModule

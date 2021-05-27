50f41029856436a1a9e0cd916e72d3129bfdd877
2zqa <25235249+2zqa@users.noreply.github.com>
2021-05-27 13:46:32 +0200

    Add kitchen to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginscreenComponent } from './loginscreen/loginscreen.component';
 import { FloorplanComponent } from './floorplan/floorplan.component';
+import { KitchenComponent } from './kitchen/kitchen.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginscreenComponent },
   { path: 'navigation', component: FloorplanComponent },
+  { path: 'kitchen', component: KitchenComponent },
 ];
 
 @NgModule({

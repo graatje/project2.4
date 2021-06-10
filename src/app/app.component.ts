7b9a81d952cd63116e28fa565a7c86d092c0d658
2zqa <25235249+2zqa@users.noreply.github.com>
2021-06-10 17:42:08 +0200

    Implement PWA (see commit description)
    app.module.ts: registration is nu registerImmediately omdat de app nooit stable is. Moet registerWhenStable:30000 zijn.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {ApplicationRef, Component, OnInit} from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
+import {of} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'StuDorm';
+  isInstalled = this.swUpdate.isEnabled;
+  isStable = true;
+
+  ngOnInit(): void {
+    this.ref.isStable.subscribe((isStable => this.isStable = isStable));
+  }
+
+  constructor(private swUpdate: SwUpdate, private ref: ApplicationRef) {}
 }

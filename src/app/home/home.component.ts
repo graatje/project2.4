268f749a62e5ef119a158994eca6e9589cf0f12a
mlandHanze <81427710+mlandHanze@users.noreply.github.com>
2021-06-30 16:39:13 +0200

    Small redirection fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService : AuthService,
+    private router : Router) { }
 
   ngOnInit(): void {
+    if (this.authService.isLoggedIn()){
+      this.router.navigate(['plattegrond']);
+    }
   }
 
 }
